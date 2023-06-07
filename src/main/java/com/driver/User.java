f0996d670f6f49acc22c60b4d0ea2687582f020e
sunil kumar <105sunil2000@gmail.com>
2023-06-07 12:28:08 +0530

    changes done

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -1,6 +1,50 @@
 package com.driver;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+
+import java.util.Objects;
+
 public class User {
     private String name;
     private String mobile;
+
+    //constructors
+    public User(){
+
+    }
+    public User(String name,String mobile){
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    //Getters and Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    //overriden methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getName(), user.getName()) && Objects.equals(getMobile(), user.getMobile());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getMobile());
+    }
 }
